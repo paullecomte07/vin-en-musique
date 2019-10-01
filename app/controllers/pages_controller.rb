49cf0f63e16fe5bc6a3acea4af2b13a595d53242
Paul Lecomte <paullecomte07@gmail.com>
2019-10-01 23:15:43 +0200

    dashboard and home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :crus, :environnement, :terroir, :histoire, :innovation, :art]
 
   def home
   end
+
+  def crus
+  end
+
+  def environnement
+  end
+
+  def terroir
+  end
+
+  def histoire
+  end
+
+  def innovation
+  end
+
+  def art
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'crus', to:'pages#crus', as: :crus
+  get 'histoire',to:'pages#histoire', as: :histoire
+  get 'terroir',to:'pages#terroir', as: :terroir
+  get 'environnement',to:'pages#environnement', as: :environnement
+  get 'innovation',to:'pages#innovation', as: :innovation
+  get 'art',to:'pages#art', as: :art
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
