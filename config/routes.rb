9992a17d24c89edb60b1934141a06c0a7b8065f4
Paul Lecomte <paullecomte07@gmail.com>
2019-10-12 19:02:58 +0200

    home page design

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'crus', to:'pages#crus', as: :crus
-  get 'histoire',to:'pages#histoire', as: :histoire
+  get 'home',to:'pages#home', as: :home
   get 'terroir',to:'pages#terroir', as: :terroir
   get 'environnement',to:'pages#environnement', as: :environnement
   get 'innovation',to:'pages#innovation', as: :innovation
