e573513c64351e091980eeb6221ddb60140eb43f
Paul Lecomte <paullecomte07@gmail.com>
2019-11-02 14:14:21 +0100

    refresh on the design of home, vine, engagements, terroir pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'crus', to:'pages#crus', as: :crus
+  get 'vin', to:'pages#vin', as: :vin
   get 'home',to:'pages#home', as: :home
   get 'terroir',to:'pages#terroir', as: :terroir
-  get 'environnement',to:'pages#environnement', as: :environnement
+  get 'engagement',to:'pages#engagement', as: :engagement
   get 'histoire',to:'pages#histoire', as: :histoire
-  get 'art',to:'pages#art', as: :art
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
